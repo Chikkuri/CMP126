0e9dedbf746d0862d36265f07637cddb77bac89a
Chikkuri <9jays8@gmail.com>
2017-02-24 15:44:44 -0500

    Create MixingDataTypesDemo.java

---
+++ MixingDataTypesDemo.java
@@ -0,0 +1,18 @@
+// MixingDatatypesDemo
+
+public class MixingDataTypesDemo {
+	public static void main (String[] args) {
+
+		int x;
+		double y;
+
+		x=10;
+		y=7;
+
+//	x=y;
+		y=x;
+		
+		System.out.println("x has: " + x + ", and y has: " + y);
+
+	}
+}
